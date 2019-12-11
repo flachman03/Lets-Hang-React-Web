5e24692ddd56e632c82dad0253da5986279b78f5
RyanFlachman <ryan@flachman.net>
2019-12-11 12:28:17 -0700

    Add fields to state for signup information

---
+++ src/Components/SignUpForm/SignUpForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class SignUpForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      userName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
