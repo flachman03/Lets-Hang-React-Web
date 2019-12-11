f5836a7f47385b91f6a77fa4eef56a97d83ca67f
RyanFlachman <ryan@flachman.net>
2019-12-11 12:33:43 -0700

    Create handleOnchange method for SignUpForm

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -11,9 +11,20 @@ export class SignUpForm extends React.Component {
       confirmPassword: ''
     }
   }
+
+  handleOnChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
   render(){
     return(
-      <div></div>
+      <form>
+        <input 
+          type='text'
+          name='name'
+          value={this.state.name}
+          onChange={(e) => this.handleOnChange(e)}>
+        </input>
+      </form>
     )
   }
 }
