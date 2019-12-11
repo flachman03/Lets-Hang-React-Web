feff7c5855e8a5058fab2449a7bfea1271d30c7d
RyanFlachman <ryan@flachman.net>
2019-12-11 16:10:11 -0700

    Write basic tests for SignUpForm

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './SignUpForm.scss'
+
 
 export default class SignUpForm extends React.Component {
   constructor() {
@@ -18,52 +20,65 @@ export default class SignUpForm extends React.Component {
   }
   render(){
     return(
-      <form className='signupform__header'>
+      <form className='signupform'>
         <input 
           className='signupform__input'
+          id='signupform__input--name'
           type='text'
           name='name'
+          placeholder='Firstname Lastname'
           value={this.state.name}
           onChange={(e) => this.handleOnChange(e)}>
         </input>
         <input
           className='signupform__input'
+          id='signupform__input--username'
           type='text'
           name='userName'
+          placeholder='User Name'
           value={this.state.userName}
           onChange={(e) => this.handleOnChange(e)}
         >
         </input>
         <input
           className='signupform__input'
+          id='signupform__input--email'
           type='text'
           name='email'
+          placeholder='Email eg. user@gmail.com'
           value={this.state.email}
           onChange={(e) => this.handleOnChange(e)}>
         </input>
         <input
           className='signupform__input'
+          id='signupform__input--phonenumber'
           type='text'
           name='phoneNumber'
+          placeholder='Phone Number eg. 3033333333'
           value={this.state.phoneNumber}
           onChange={ e => this.handleOnChange(e)}>
         </input>
         <input
           className='signupform__input'
-          type='text'
+          id='signupform__input--password'
+          type='password'
           name='password'
+          placeholder='Password'
           value={this.state.password}
           onChange={ e => this.handleOnChange(e)}>
         </input>
         <input
           className='signupform__input'
+          id='signupform__input--confirm'
           type='text'
           name='confirmPassword'
+          placeholder='Confirm Password'
           value={this.state.confirmPassword}
           onChange={ e => this.handleOnChange(e)}>
         </input>
         <button
           className='signupform__submitbtn'
+          placeholder='Submit'
           type='submit'>
         </button>
       </form>
