9d5b9e151601821cf9016291195529f7c60cd36e
RyanFlachman <ryan@flachman.net>
2019-12-11 17:43:35 -0700

    Add UserReducer and RootReducer to create inital redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { compostWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
 
