4e584f304369fc22ab20042ff07b0a5ae29e8467
RyanFlachman <ryan@flachman.net>
2019-12-11 17:47:00 -0700

    Add rootReducer to createStore in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { compostWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './Reducers/index'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 
-const store = createStore();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 <Provider store={store}>
