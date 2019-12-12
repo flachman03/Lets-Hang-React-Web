2aeafb3fbf969266afa8ca0f18a0af695c8c6cf4
RyanFlachman <ryan@flachman.net>
2019-12-12 12:12:46 -0700

    Add error and loading reducer to rootReducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { UserReducer } from './UserReducer'
+import { LoadingReducer } from './LoadingReducer';
+import { HasErroredReducer } from './ErrorReducer';
 
 export const rootReducer = combineReducers({
-  user: UserReducer
+  user: UserReducer,
+  isLoading: LoadingReducer,
+  error: HasErroredReducer
 });
 
 export default rootReducer;
