f947a56e42017f11c4400baa45fbf03c14aad6c9
RyanFlachman <ryan@flachman.net>
2019-12-11 18:37:05 -0700

    Added a CreateUserThunk

---
+++ src/Thunks/UserThunks/CreateUserThunk.js
@@ -0,0 +1,24 @@
+import { addUser } from '../../Actions/UserActions'
+
+export const CreateUserThunk = userInfo => {
+  const url = 'https://localhost:5001/api/v1/user'
+  return async dispatch => {
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(userInfo)
+      };
+      const response = await fetch(url, options);
+      const user = await response.json();
+      dispatch(addUser(user))
+      return user;
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+}
