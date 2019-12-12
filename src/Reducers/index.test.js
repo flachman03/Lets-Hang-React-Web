ac198630bd5a388e3652bc4420782e263ebe4298
RyanFlachman <ryan@flachman.net>
2019-12-12 13:44:52 -0700

    Add rootReducer unit test

---
+++ src/Reducers/index.test.js
@@ -0,0 +1,10 @@
+import { rootReducer } from './index'
+
+describe('rooReducer', () => {
+  it('Should return an object with a each reducer', () => {
+
+    const result = rootReducer
+
+    expect( result ).toContain()
+  })
+})
