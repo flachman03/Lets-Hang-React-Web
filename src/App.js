bae6c7f266356a347b8e9f3844a269db426e65c5
Steve Rumizen <steverumizen@gmail.com>
2019-12-13 12:42:59 -0700

    Add bones of LoginForm component and render it within App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import SignUpForm from './Components/SignUpForm/SignUpForm'
+import SignUpForm from './Components/SignUpForm/SignUpForm';
+import LoginForm from "./Components/LoginForm/LoginForm";
 import './App.css';
 
 function App() {
   return (
     <main>
+      <LoginForm />
       <SignUpForm />
     </main>
   )
---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./LoginForm.scss";
+import { connect } from "react-redux";
+import { CreateUserThunk } from "../../Thunks/UserThunks/CreateUserThunk";
+
+export class LoginForm extends React.PureComponent {
+  state = {
+    userName: "",
+    password: ""
+  };
+
+  handleOnChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  // handleCreateUser = e => {
+  //   e.preventDefault();
+  //   const user = {
+  //     name: this.state.name,
+  //     userName: this.state.userName,
+  //     email: this.state.email,
+  //     phoneNumber: this.state.phoneNumber,
+  //     password: this.state.password,
+  //     confirmPassword: this.state.confirmPassword
+  //   };
+  //   this.props.createUser(user);
+  // };
+
+  render() {
+    return (
+      <form className="loginform">
+        <input
+          className="loginform__input"
+          id="loginform__input--username"
+          type="text"
+          name="username"
+          placeholder="Username or Email"
+          value={this.state.username}
+          onChange={e => this.handleOnChange(e)}
+        ></input>
+        <input
+          className="loginform__input"
+          id="loginform__input--password"
+          type="text"
+          name="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={e => this.handleOnChange(e)}
+        ></input>
+        <button
+          className="loginform__submitbtn"
+          placeholder="Login"
+          type="submit"
+          onClick={e => this.handleCreateUser(e)}
+        ></button>
+      </form>
+    );
+  }
+}
+
+export const mapDispatchToProps = dispatch => ({
+  createUser: user => dispatch(CreateUserThunk(user))
+});
+
+export default connect(null, mapDispatchToProps)(LoginForm);
