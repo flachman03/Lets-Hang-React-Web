d9f1c63e92a2706741b4a23e4bbd87e7a4e616f2
Steve Rumizen <steverumizen@gmail.com>
2019-12-13 14:07:20 -0700

    Add background image opacity and fine tune placement of inputs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import SignUpForm from './Components/SignUpForm/SignUpForm';
+import React from "react";
+import SignUpForm from "./Components/SignUpForm/SignUpForm";
 import LoginForm from "./Components/LoginForm/LoginForm";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <main>
+      <header className="header">
+        <h1 className="lets-hang">Let's Hang</h1>
+      </header>
       <LoginForm />
-      <SignUpForm />
+      {/* <SignUpForm /> */}
     </main>
-  )
+  );
 }
 
 export default App;
