3675053b677e25b55264aea10a8ad6789cf5e7f2
RyanFlachman <ryan@flachman.net>
2019-12-14 11:15:20 -0700

    Add GetUserEventThunk

---
+++ src/Thunks/EventThunks/GetUserEventThunk.js
@@ -0,0 +1,17 @@
+import { userEvents } from '../../Actions/EventActions'
+import { hasErrored} from '../../Actions/index'
+
+export const GetUserEventThunk = ApiKey => {
+  const url = `https://localhost:5001/api/v1/event/current?ApiKey=${ApiKey}`
+  return async dispatch => {
+    try {
+      const response = await fetch(url);
+      const event = await response.json();
+      dispatch(userEvents(event));
+      return event
+    }
+    catch(error) {
+      dispatch(hasErrored(error))
+    }
+  }
+}
