a9a05cc67eb2b3f671a0bea55b33ebcbe4434e2b
RyanFlachman <ryan@flachman.net>
2019-12-16 10:05:09 -0700

    Add structure for EventsContainer

---
+++ src/Components/EventsContainer/EventsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class EventsContainer extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
