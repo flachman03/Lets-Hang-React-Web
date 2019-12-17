d8dcda15f6044d0a6fccf798fa133bded78a3c7a
RyanFlachman <ryan@flachman.net>
2019-12-16 17:11:52 -0700

    Add getFriendsThunk

---
+++ src/Thunks/FriendThunks/getFriendsThunk.js
@@ -0,0 +1,18 @@
+import { addFriends } from '../../Actions/FriendActions'
+import { hasErrored } from '../../Actions';
+
+export const getFriendsThunk = apiKey => {
+  const url = `https://localhost5001/api/v1/user/friends?=ApiKey=${apiKey}`
+  
+  return async dispatch => {
+    try {
+      const response = await fetch(url);
+      const friends = await response.json();
+      dispatch(addFriends(friends));
+      return friends  
+    }
+    catch(error) {
+      dispatch(hasErrored(error))
+    }
+  }
+}
